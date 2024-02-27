2552e354acbb95d82f98ad0fc4e8749200c1e15b
Hyungi Kim <hyungi0731@gmail.com>
2024-02-27 17:02:28 -0500

    Update 0180-consecutive-numbers.sql

--- Medium/0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ Medium/0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,2 +1,13 @@
 # Write your MySQL query statement below
 select distinct num as ConsecutiveNums from Logs where (id+1,num) in (select * from logs) and (id+2,num) in (select * from logs)
+
+WAY 2)
+with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
+
+)
+
+select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
